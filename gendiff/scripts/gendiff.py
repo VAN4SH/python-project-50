80f1eff56e75e373dd5643bf3d7bc94dd7c23c04
ivan <ivan@dmitriev-o2k.o2k.ru>
2024-07-17 09:32:45 +0300

    add new files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,7 +5,9 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', type=str, help='set format of output', default='plain')
     args = parser.parse_args()
+    print(f"Comparing {args.first_file} and {args.second_file} with format: {args.format}")
 
 
 if __name__ == "__main__":
