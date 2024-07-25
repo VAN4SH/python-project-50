4840d002b54463d7b8f834fff750c872ed03230c
ivan <ivan@dmitriev-o2k.o2k.ru>
2024-07-25 13:46:41 +0300

    add new files

---
+++ gendiff/parser.py
@@ -0,0 +1,14 @@
+import json
+import yaml
+
+def parse(data, file_format):
+    if file_format == 'json':
+        return json.loads(data)
+    elif file_format in ['yml', 'yaml']:
+        return yaml.safe_load(data)
+    else:
+        raise ValueError(f"Unsupported file format: {file_format}")
+
+def get_file_format(filepath):
+    _, ext = os.path.splitext(filepath)
+    return ext[1:] if ext else None
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,14 +1,20 @@
 import argparse
-
+from gendiff.gendiff import generate_diff
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format', type=str, help='set format of output', default='plain')
+    parser = argparse.ArgumentParser(description="Generate diff between two files.")
+    parser.add_argument("first_file", help="path to the first file")
+    parser.add_argument("second_file", help="path to the second file")
+    parser.add_argument(
+        "-f",
+        "--format",
+        default="stylish",
+        help="set format of output (default: stylish)")
+    
     args = parser.parse_args()
-    print(f"Comparing {args.first_file} and {args.second_file} with format: {args.format}")
-
+    
+    diff = generate_diff(args.first_file, args.second_file, args.format)
+    print(diff)
 
 if __name__ == "__main__":
     main()
